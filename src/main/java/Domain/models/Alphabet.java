b7cd34e1fb2ddabb5195b23b60d2299d682405c0
Alexander Wierer <Alexander.Wierer@stud.fh-rosenheim.de>
2020-04-08 16:25:47 +0200

    schritt2 fertig

--- src/main/java/Domain/models/Alphabet.java
+++ src/main/java/Domain/models/Alphabet.java
@@ -1,8 +1,26 @@
 package Domain.models;
 
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Alphabet {
     CREATE,
     DELETE,
     SYNC,
-    MODIFY
+    MODIFY;
+
+    public static Map<WatchEvent.Kind<?>, Alphabet> watchEventToAlphabet = new HashMap<>();
+
+    static {
+        watchEventToAlphabet.put(StandardWatchEventKinds.ENTRY_CREATE, CREATE);
+        watchEventToAlphabet.put(StandardWatchEventKinds.ENTRY_MODIFY, MODIFY);
+        watchEventToAlphabet.put(StandardWatchEventKinds.ENTRY_DELETE, DELETE);
+    }
+
+    public static Alphabet convertWatchEvents(WatchEvent.Kind<?> eventKinds) {
+        return watchEventToAlphabet.get(eventKinds);
+    }
 }
