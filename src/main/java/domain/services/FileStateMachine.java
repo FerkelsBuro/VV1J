70cab84b4755f5190841f3994d21e1ca3de6ea18
Alexander Wierer <Alexander.Wierer@stud.fh-rosenheim.de>
2020-05-03 00:57:05 +0200

    formating

--- src/main/java/domain/services/FileStateMachine.java
+++ src/main/java/domain/services/FileStateMachine.java
@@ -9,22 +9,19 @@ import java.util.EnumMap;
 public class FileStateMachine {
     private static final EnumMap<Status, EnumMap<Alphabet, Status>> stateMachine = new EnumMap<>(Status.class);
 
-
     static {
         addStatusReaction(Status.CREATED, Status.CREATED, Status.GONE, Status.CREATED, Status.INSYNC);
         addStatusReaction(Status.DELETED, Status.MODIFIED, Status.DELETED, Status.MODIFIED, Status.GONE);
         addStatusReaction(Status.GONE, Status.CREATED, Status.GONE, Status.GONE, Status.GONE);
         addStatusReaction(Status.INSYNC, Status.MODIFIED, Status.DELETED, Status.MODIFIED, Status.INSYNC);
         addStatusReaction(Status.MODIFIED, Status.MODIFIED, Status.DELETED, Status.MODIFIED, Status.INSYNC);
-
     }
 
     private FileStateMachine() {
     }
 
-    private static void addStatusReaction(Status currentStatus, Status afterCreate,
-                                          Status afterDelete, Status afterModify,
-                                          Status afterSync) {
+    private static void addStatusReaction(Status currentStatus, Status afterCreate, Status afterDelete,
+                                          Status afterModify, Status afterSync) {
         EnumMap<Alphabet, Status> temp = new EnumMap<>(Alphabet.class);
         temp.put(Alphabet.CREATE, afterCreate);
         temp.put(Alphabet.DELETE, afterDelete);
