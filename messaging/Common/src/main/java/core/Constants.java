f41079997f788c7d009bbf000f2ef170bc95d774
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-05-21 14:59:47 +0200

    added queue names as constants

---
+++ messaging/Common/src/main/java/core/Constants.java
@@ -0,0 +1,10 @@
+package core;
+
+public class Constants {
+    public static class Queues {
+        public static final String OPEN_ORDERS = "OpenOrders";
+        public static final String NEED_APPROVAL = "NeedsApproval";
+        public static final String APPROVED_ORDERS = "APPROVED_ORDERS";
+        public static final String DECLINED_ORDER = "DECLINED_ORDER";
+    }
+}
