d13ff825126d46562a6ce5393018e95b82485510
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-06-07 14:20:55 +0200

    CustomerService uses a configurable interval

--- messaging/CustomerService/src/main/java/starter/Startup.java
+++ messaging/CustomerService/src/main/java/starter/Startup.java
@@ -10,9 +10,16 @@ import java.util.concurrent.TimeoutException;
 
 public class Startup {
     private final static Gson gson = new Gson();
+    private final static int DEFAULT_INTERVAL_IN_MILLISECONDS = 1000;
 
-    public static void main(String[] argv) throws IOException, TimeoutException {
+    public static void main(String[] argv) throws IOException, TimeoutException, InterruptedException {
+        int interval = argv.length > 0 ? Integer.parseInt(argv[0]) : DEFAULT_INTERVAL_IN_MILLISECONDS;
+
         MessageSender messageSender = new MessageSender(gson);
-        messageSender.send(Constants.Queues.OPEN_ORDERS, RandomOrderGenerator.createRandomOrder());
+
+        while(!Thread.interrupted()) {
+            messageSender.send(Constants.Queues.OPEN_ORDERS, RandomOrderGenerator.createRandomOrder());
+            Thread.sleep(interval);
+        }
     }
 }
