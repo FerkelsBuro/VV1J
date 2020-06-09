85c84597cdd6a167f6190a33af786cd4d52bd17d
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-06-09 11:03:31 +0200

    fixed maximum_amount for a random order

--- messaging/CustomerService/src/main/java/services/RandomOrderGenerator.java
+++ messaging/CustomerService/src/main/java/services/RandomOrderGenerator.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomOrderGenerator {
     private static final int MINIMUM_AMOUNT = 1;
-    private static final int MAXIMUM_AMOUNT = 20_000;
+    private static final int MAXIMUM_AMOUNT = 2_000;
 
     public static Order createRandomOrder() {
         Customer customer = new Customer("Company", "Michael", "Bacher", "Michael.Bacher@Grunzenham.com");
