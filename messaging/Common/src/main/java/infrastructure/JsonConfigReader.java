84aac22727a2e6fa625cd23dca5721861f9a106e
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-06-09 11:02:56 +0200

    each project now has its own config.json

--- messaging/Common/src/main/java/infrastructure/JsonConfigReader.java
+++ messaging/Common/src/main/java/infrastructure/JsonConfigReader.java
@@ -11,8 +11,8 @@ import java.io.FileReader;
 import java.nio.file.Paths;
 
 public class JsonConfigReader {
-    public static final String DEFAULT_CONFIG_PATH = Paths.get(System.getProperty("user.dir")).getParent()
-            + File.separator + "Common" + File.separator + "config.json";
+    public static final String DEFAULT_CONFIG_PATH = Paths.get(System.getProperty("user.dir"))
+            + File.separator + "config.json";
 
     public static void readConfigJson(String configPath, ConnectionFactory factory) throws FileNotFoundException {
         JsonElement jsonElement = JsonParser.parseReader(new FileReader(configPath));
