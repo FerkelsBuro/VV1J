4fc9cfe8f5799e6a2b01cbf42ad6df300b2ef75c
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-06-15 14:06:36 +0200

    MessageReceiver works again

--- messaging/Common/src/main/java/infrastructure/MessageReceiver.java
+++ messaging/Common/src/main/java/infrastructure/MessageReceiver.java
@@ -7,7 +7,6 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import core.loggers.StaticLogger;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
@@ -19,15 +18,19 @@ import java.util.function.Consumer;
  */
 public class MessageReceiver {
     private final ConnectionFactory factory = new ConnectionFactory();
+    private final Connection connection;
+    private final Channel channel;
     private final Gson gson;
 
-    public MessageReceiver(Gson gson) throws FileNotFoundException {
+    public MessageReceiver(Gson gson) throws IOException, TimeoutException {
         this(gson, JsonConfigReader.DEFAULT_CONFIG_PATH);
     }
 
-    public MessageReceiver(Gson gson, String configPath) throws FileNotFoundException {
+    public MessageReceiver(Gson gson, String configPath) throws IOException, TimeoutException {
         this.gson = gson;
         JsonConfigReader.readConfigJson(configPath, this.factory);
+        connection = factory.newConnection();
+        channel = connection.createChannel();
     }
 
     /**
@@ -39,26 +42,21 @@ public class MessageReceiver {
      * @param clazz            class of the message
      * @param <T>
      * @throws IOException
-     * @throws TimeoutException
      */
-    public <T> void receive(String exchangeName, String queueName, Consumer<T> onMessageReceive, Class<T> clazz) throws IOException, TimeoutException {
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+    public <T> void receive(String exchangeName, String queueName, Consumer<T> onMessageReceive, Class<T> clazz) throws IOException {
+        channel.exchangeDeclare(exchangeName, "fanout");
+        channel.queueDeclare(queueName, true, false, false, null);
+        channel.queueBind(queueName, exchangeName, "");
 
-            channel.exchangeDeclare(exchangeName, "fanout");
-            channel.queueDeclare(queueName, true, false, false, null);
-            channel.queueBind(queueName, exchangeName, "");
+        StaticLogger.logger.info(" [*] Waiting for messages. To exit press CTRL+C\n");
 
-            StaticLogger.logger.info(" [*] Waiting for messages. To exit press CTRL+C\n");
-
-            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-                T order = gson.fromJson(message, (Type) clazz);
-                StaticLogger.logger.info(() -> " [x] Received from " + exchangeName + " '" + message + "'");
-                onMessageReceive.accept(order);
-            };
-            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
-            });
-        }
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            T order = gson.fromJson(message, (Type) clazz);
+            StaticLogger.logger.info(() -> " [x] Received from " + exchangeName + " '" + message + "'");
+            onMessageReceive.accept(order);
+        };
+        channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
+        });
     }
 }
