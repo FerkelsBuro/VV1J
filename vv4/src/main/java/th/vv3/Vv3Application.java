c12dc86e51d67eedc0755f78c41ea1687e8de5ba
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-06-28 14:45:08 +0200

    added VV4-project

---
+++ vv4/src/main/java/th/vv3/Vv3Application.java
@@ -0,0 +1,13 @@
+package th.vv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vv3Application.class, args);
+    }
+
+}
