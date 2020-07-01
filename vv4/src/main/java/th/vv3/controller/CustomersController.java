c79a20b3181594b2dc383bde27ed6f488bb9634a
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-07-01 15:12:49 +0200

    Customer Controller put returns proper status codes

--- vv4/src/main/java/th/vv3/controller/CustomersController.java
+++ vv4/src/main/java/th/vv3/controller/CustomersController.java
@@ -1,6 +1,8 @@
 package th.vv3.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import th.vv3.models.Customer;
 import th.vv3.repositories.CustomerRepository;
@@ -29,14 +31,15 @@ public class CustomersController {
     }
 
     @PutMapping
-    public Customer change(@RequestBody Customer customer) {
+    public ResponseEntity change(@RequestBody Customer customer) {
         if (customer.getCustomerId() == null) {
-            return null;
+            return new ResponseEntity<>("id required", HttpStatus.BAD_REQUEST);
         }
         if (customerRepository.findById(customer.getCustomerId()).equals(Optional.empty())) {
-            return null;
+            return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND);
         }
+
         customerRepository.save(customer);
-        return customer;
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 }
