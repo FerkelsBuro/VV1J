62aa9e4df8d64a9ff21bb708c8161b1fa3361c85
Alexander Wierer <alexander.wierer@stud.fh-rosenheim.de>
2020-07-01 13:57:24 +0200

    added Order class

---
+++ vv4/src/main/java/th/vv3/models/Order.java
@@ -0,0 +1,66 @@
+package th.vv3.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Order with an UUID
+ */
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue
+    private UUID orderId;
+    private int amount;
+    private Date createDate;
+    private Customer customer;
+    private String approvedBy;
+
+    public Order(int amount, Customer customer) {
+        orderId = UUID.randomUUID();
+        this.amount = amount;
+        this.customer = customer;
+        createDate = new Date(System.currentTimeMillis());
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return orderId.equals(order.orderId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orderId);
+    }
+
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
+    }
+}
